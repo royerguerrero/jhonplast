90041eed0cae2214dd922282238993a6462f09b1
Royer G <royjuni3431@gmail.com>
2021-07-09 18:31:27 -0500

    feat: add basic structure to project

---
+++ static/javascript/tabs.js
@@ -0,0 +1,6 @@
+const tabs = document.querySelectorAll('.tabs')
+
+tabs.forEach(tab => {
+    const tabItems = tabs.querySelectorAll('.tabs__items')
+    const tabParagraphs = tabs.querySelectorAll('.tabs__paragraph')
+})
