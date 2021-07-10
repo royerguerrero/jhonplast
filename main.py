15eccc929628ef65e1f32e3b66c59708a3d0c745
Royer G <royjuni3431@gmail.com>
2021-07-09 19:00:27 -0500

    feat: add contact page and products view

--- main.py
+++ main.py
@@ -5,3 +5,11 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/contacto/')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/productos/<category>/')
+def products(category):
+    return f'Category: {category}'
