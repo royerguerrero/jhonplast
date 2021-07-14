7ec669b310954f26ce13ede7a1ba2e6fb21b85c5
Royer G <royjuni3431@gmail.com>
2021-07-13 20:43:02 -0500

    feat: add pymongo and refactor architectura to the app

--- app/__init__.py
+++ app/__init__.py
@@ -1,21 +1,29 @@
+import os
+
 from flask import Flask, render_template, request
+
+from app.extensions import mongo
 from app.api import api
 
 
-app = Flask(__name__)
-app.register_blueprint(api)
+def create_app(config_object='app.settings'):
+    app = Flask(__name__)
+    app.config.from_object(config_object)
 
+    mongo.init_app(app)
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+    app.register_blueprint(api)
 
+    @app.route('/')
+    def index():
+        return render_template('index.html')
 
-@app.route('/contacto/')
-def contact():
-    return render_template('contact.html')
+    @app.route('/contacto/')
+    def contact():
+        return render_template('contact.html')
 
+    @app.route('/productos/')
+    def products():
+        return render_template('products.html')
 
-@app.route('/productos/')
-def products():
-    return render_template('products.html')
+    return app
--- app/api.py
+++ app/api.py
@@ -1,8 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, Response
+from app.extensions import mongo
+from bson import json_util
 
 api = Blueprint('api', __name__, url_prefix='/api/')
 
 
 @api.route('/products/', methods=['GET'])
 def products():
-    return 'Hi from products endpoint'
+    return 'Products endpoint...'
