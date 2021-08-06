c1cb7cdfb52f2fc0be3ce28886fa5bc1dd3c82c7
Royer G <royjuni3431@gmail.com>
2021-08-06 14:31:25 -0500

    feat: add capacity and more thing

--- app/static/javascript/menu.js
+++ app/static/javascript/menu.js
@@ -1,8 +1,12 @@
 const menuItems = document.querySelectorAll('.navbar__item')
 
-
 menuItems.forEach(item => {
     if (item.href == window.location) {
         item.classList.add('navbar__item--active')
     }
+})
+
+const burgerButton = document.querySelector('.navbar__toogle-button')
+burgerButton.addEventListener('click', () => {
+    // TODO: Add logic for show or not the navbar
 })
