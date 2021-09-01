dc4413b5afbf256b703956ec5241a8f59432d248
Royer G <royjuni3431@gmail.com>
2021-08-31 20:18:14 -0500

    feat: capitalize name of products

--- app/extensions.py
+++ app/extensions.py
@@ -1,5 +1,4 @@
 from flask_mail import Mail
-from flask_pymongo import PyMongo
 from flask_sqlalchemy import SQLAlchemy
 
 db = SQLAlchemy()
--- app/utils/load_products.py
+++ app/utils/load_products.py
@@ -1,11 +1,12 @@
 import csv
 from app.models import Product
 
+
 def run():
     """
     Reads the products.csv and returns a list of serialized products
     to be appened into db
-    
+
     Note: To run this script you must import it from flask shell
     """
     with open('products.csv', mode='r') as csv_file:
@@ -15,11 +16,13 @@ def run():
         for row in csv_reader:
             if line_count == 0:
                 line_count += 1
-            
+
             if row['capacity'] == '':
-                products.append(Product(name=row['name'], price=row['price'], image=row['image'], heigth=row['dimentions.heigth'], width=row['dimentions.width'], length=row['dimentions.length'], category=row['category']))
+                products.append(Product(name=row['name'].capitalize(), price=row['price'], image=row['image'], heigth=row['dimentions.heigth'],
+                                width=row['dimentions.width'], length=row['dimentions.length'], category=row['category']))
             else:
-                products.append(Product(name=row['name'], price=row['price'], image=row['image'], capacity=row['capacity'], category=row['category']))
+                products.append(Product(
+                    name=row['name'].capitalize(), price=row['price'], image=row['image'], capacity=row['capacity'], category=row['category']))
 
             line_count += 1
 
